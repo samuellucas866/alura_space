8ab00f4238d693bfdbb588ce866c88ae53c900a5
samuellucas866 <samuellucas188@gmail.com>
2024-08-23 11:19:23 -0300

    Alterações no login, cadastro e autenticação

--- galeria/admin.py
+++ galeria/admin.py
@@ -6,9 +6,10 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda", "publicada") 
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
-    list_filter = ("categoria",)
+    list_filter = ("categoria", "usuario",)
     list_editable=("publicada",)
     list_per_page = 10
+    
 
 
 admin.site.register(Fotografia, ListandoFotografias)
